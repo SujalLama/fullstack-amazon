95211eae4a6817db9ab76ee6c6f174e6b769fa69
sujal lama <lsujal41@gmail.com>
2023-09-11 16:07:58 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+// Import the API category from AWS Amplify
+import './App.css';
+import Chapter2 from "./chapters/Chapter2";
+
+function App() {
+  return (
+    <>
+    <Chapter2 />
+    </>
+); }
+
+export default App
