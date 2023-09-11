823489187bd8772022550413615033a3e6747a05
sujal lama <lsujal41@gmail.com>
2023-09-11 20:38:38 +0545

    graphql api integration

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,6 +4,11 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
+    },
+    "notesapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
   },
   "function": {
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 // Import the API category from AWS Amplify
 import './App.css';
 import Chapter2 from "./chapters/chapter2";
+import Chapter3 from './chapters/chapter3';
 
 function App() {
   return (
     <>
-    <Chapter2 />
+    {/* <Chapter2 /> */}
+    <Chapter3 />
     </>
 ); }
 
